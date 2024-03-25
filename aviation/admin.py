7ead8364c46e4daaeda1f5972305e4b0167295ce
TP <pktai.iot@gmail.com>
2024-03-25 10:41:00 +0700

    add some field to add booking form

--- aviation/admin.py
+++ aviation/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Flight, Aircraft, Booking, Passenger, PaymentInformation,  ItemInformation, CarryOnItem, AircraftAdmin, PassengerAdmin, FlightAdmin
+from .models import Flight, Aircraft, Booking, Passenger, PaymentInformation,  ItemInformation, CarryOnItem, AircraftAdmin, PassengerAdmin, FlightAdmin, BookingAdmin
 
 admin.site.register(Flight, FlightAdmin)
 admin.site.register(Aircraft, AircraftAdmin)
-admin.site.register(Booking)
+admin.site.register(Booking, BookingAdmin)
 admin.site.register(Passenger, PassengerAdmin)
 admin.site.register(PaymentInformation)
 admin.site.register(ItemInformation)
