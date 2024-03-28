29ec848da12e72ccd631cfde96bde4b4cbeead7f
TP <pktai.iot@gmail.com>
2024-03-28 09:37:30 +0700

    refactor mock data

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,3 +1,6 @@
+python ./scripts/aircraft.py
+python ./scripts/passenger.py
+python ./scripts/flight.py
 python manage.py loaddata 0001_Aircraft.json
 python manage.py loaddata 0002_Passenger.json
 python manage.py loaddata 0003_Flight.json
