92bbf3ab9481334b43c6b7db31c458b12c0ea2ef
TP <pktai.iot@gmail.com>
2024-04-03 22:52:05 +0700

    appy decorator

--- aviation/admin.py
+++ aviation/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from datetime import timedelta
 
 from aviation.forms import BookingForm
-
 from .models import Flight, Aircraft, Booking, Passenger
 
 
+@admin.register(Flight)
 class FlightAdmin(admin.ModelAdmin):
     list_display = [
         "id",
@@ -27,81 +27,87 @@ class FlightAdmin(admin.ModelAdmin):
     ]
     list_filter = ["departure_airport", "arrival_airport"]
 
-    def aircraft_code(self, obj):
+    @staticmethod
+    def aircraft_code(obj):
         return obj.aircraft.code if obj.aircraft else ""
 
-    def duration_time(self, obj):
+    @staticmethod
+    def duration_time(obj):
         if obj.departure_time and obj.arrival_time:
             duration = obj.arrival_time - obj.departure_time
             return duration
         else:
             return None
 
+    aircraft_code.short_description = "Aircraft Code"
+    duration_time.short_description = "Duration"
 
+
+@admin.register(Aircraft)
 class AircraftAdmin(admin.ModelAdmin):
     list_display = ["id", "model", "code", "capacity"]
     search_fields = ["id", "model", "code", "capacity"]
     list_filter = ["model"]
 
 
+@admin.register(Passenger)
 class PassengerAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "email", "phone"]
     search_fields = ["id", "name", "email", "phone"]
     list_filter = ["name", "email", "phone"]
 
 
+@admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     form = BookingForm
     filter_horizontal = ["passengers"]
     list_display = (
         "id",
-        "get_departure_airport",
-        "get_arrival_airport",
-        "get_departure_time",
-        "get_arrival_time",
-        "get_passenger_names",
+        "departure_airport",
+        "arrival_airport",
+        "departure_time",
+        "arrival_time",
+        "passenger_names",
         "total_passenger",
         "total_amount_with_vnd",
         "booking_date",
     )
 
-    def get_departure_airport(self, obj):
+    @staticmethod
+    def departure_airport(obj):
         return obj.flight.departure_airport
 
-    def get_arrival_airport(self, obj):
+    @staticmethod
+    def arrival_airport(obj):
         return obj.flight.arrival_airport
 
-    def get_passenger_names(self, obj):
+    @staticmethod
+    def passenger_names(obj):
         return ", ".join([passenger.name for passenger in obj.passengers.all()])
 
-    def get_departure_time(self, obj):
+    @staticmethod
+    def departure_time(obj):
         departure_time = obj.flight.departure_time + timedelta(hours=7)
         return departure_time.strftime("%Y-%m-%d %H:%M")
 
-    def get_arrival_time(self, obj):
+    @staticmethod
+    def arrival_time(obj):
         arrival_time = obj.flight.arrival_time + timedelta(hours=7)
         return arrival_time.strftime("%Y-%m-%d %H:%M")
 
-    def total_passenger(self, obj):
+    @staticmethod
+    def total_passenger(obj):
         return obj.passengers.count()
 
-    def total_amount_with_vnd(self, obj):
+    @staticmethod
+    def total_amount_with_vnd(obj):
         formatted_amount = "{:,.0f}".format(obj.total_amount)  # Format with commas for thousands separators
         return f"{formatted_amount} VND"
 
-    get_departure_airport.short_description = "Departure Airport"
-    get_arrival_airport.short_description = "Arrival Airport"
-    get_passenger_names.short_description = "Passenger Names"
-    get_arrival_time.short_description = "Arrival Time"
-    get_passenger_names.short_description = "Passenger Names"
+    departure_airport.short_description = "Departure Airport"
+    arrival_airport.short_description = "Arrival Airport"
+    passenger_names.short_description = "Passenger Names"
+    arrival_time.short_description = "Arrival Time"
+    passenger_names.short_description = "Passenger Names"
     total_passenger.short_description = "Total Passenger"
     total_amount_with_vnd.short_description = "Total Amount"
-
-    class Media:
-        js = ("aviation/booking.js",)
-
-
-admin.site.register(Flight, FlightAdmin)
-admin.site.register(Aircraft, AircraftAdmin)
-admin.site.register(Booking, BookingAdmin)
-admin.site.register(Passenger, PassengerAdmin)
--- aviation/urls.py
+++ aviation/urls.py
@@ -3,10 +3,10 @@ from .views import BookingFlightView, RetrieveBookingView, CalculateFaresView
 
 urlpatterns = [
     path("flights/", BookingFlightView.as_view(), name="flights"),
+    path("fares/", CalculateFaresView.as_view(), name="calculate_fares"),
     path(
         "bookings/<int:booking_id>/",
         RetrieveBookingView.as_view(),
         name="get_booking_data",
     ),
-    path("fares/", CalculateFaresView.as_view(), name="calculate_fares"),
 ]
