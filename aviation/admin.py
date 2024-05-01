dfbae92dc3537e24133177a205362ba85aba1a61
TaiPham <pktai.iot@gmail.com>
2024-05-01 20:07:29 +0700

    add type on variable

--- aviation/admin.py
+++ aviation/admin.py
@@ -1,3 +1,4 @@
+from typing import Union
 from django.contrib import admin
 import logging
 from django.utils import timezone
@@ -12,7 +13,7 @@ logger = logging.getLogger(__name__)
 class FlightAdmin(admin.ModelAdmin):
     form = FlightForm
 
-    list_display = [
+    list_display: list[str] = [
         "id",
         "departure_airport",
         "arrival_airport",
@@ -22,23 +23,23 @@ class FlightAdmin(admin.ModelAdmin):
         "duration_time",
         "total_passenger",
     ]
-    search_fields = [
+    search_fields: list[str] = [
         "id",
     ]
-    date_hierarchy = "departure_time"
+    date_hierarchy: str = "departure_time"
 
-    list_filter = ["departure_airport", "arrival_airport", "departure_time", "arrival_time"]
-    list_per_page = 20
+    list_filter: list[str] = ["departure_airport", "arrival_airport", "departure_time", "arrival_time"]
+    list_per_page: int = 20
 
-    def total_passenger(self, obj):
+    def total_passenger(self, obj: Flight) -> int:
         return obj.booking_set.aggregate(total_passengers=Count("passengers"))["total_passengers"]
 
     @staticmethod
-    def aircraft_code(obj):
+    def aircraft_code(obj: Flight) -> str:
         return obj.aircraft.code
 
     @staticmethod
-    def duration_time(obj):
+    def duration_time(obj: Flight) -> timezone.timedelta:
         duration = obj.arrival_time - obj.departure_time
         return duration
 
@@ -46,7 +47,7 @@ class FlightAdmin(admin.ModelAdmin):
     duration_time.short_description = "Duration"
     total_passenger.short_description = "Total Passenger"
 
-    def has_change_permission(self, request, obj=None):
+    def has_change_permission(self, request, obj: Union[Flight, None] = None):
         logger.debug(
             f"has_change_permission request {request} obj {obj} user {request.user} is supper user {request.user.is_superuser}"
         )
@@ -57,7 +58,7 @@ class FlightAdmin(admin.ModelAdmin):
             return False
         return super().has_change_permission(request, obj)
 
-    def has_delete_permission(self, request, obj=None):
+    def has_delete_permission(self, request, obj: Union[Flight, None] = None):
 
         logger.debug(f"request {request} obj {obj} user {request.user} is supper user {request.user.is_superuser}")
 
@@ -70,25 +71,25 @@ class FlightAdmin(admin.ModelAdmin):
 
 @admin.register(Aircraft)
 class AircraftAdmin(admin.ModelAdmin):
-    list_display = ["id", "model", "code", "capacity"]
-    search_fields = ["id", "model", "code", "capacity"]
-    list_filter = ["model"]
-    list_per_page = 20
+    list_display: list[str] = ["id", "model", "code", "capacity"]
+    search_fields: list[str] = ["id", "model", "code", "capacity"]
+    list_filter: list[str] = ["model"]
+    list_per_page: int = 20
 
 
 @admin.register(Airport)
 class AirportAdmin(admin.ModelAdmin):
-    list_display = ["id", "code", "city", "name", "latitude", "longitude"]
-    search_fields = ["code", "city", "name"]
-    list_filter = ["code", "city"]
-    list_per_page = 20
+    list_display: list[str] = ["id", "code", "city", "name", "latitude", "longitude"]
+    search_fields: list[str] = ["code", "city", "name"]
+    list_filter: list[str] = ["code", "city"]
+    list_per_page: int = 20
 
 
 @admin.register(Passenger)
 class PassengerAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "date_of_birth", "sex", "email", "phone"]
-    search_fields = ["name", "email", "phone"]
-    list_per_page = 20
+    list_display: list[str] = ["id", "name", "date_of_birth", "sex", "email", "phone"]
+    search_fields: list[str] = ["name", "email", "phone"]
+    list_per_page: int = 20
 
 
 @admin.register(Booking)
@@ -96,7 +97,7 @@ class BookingAdmin(admin.ModelAdmin):
     form = BookingForm
     filter_horizontal = ["passengers"]
     search_fields = ["passengers__name"]
-    list_display = (
+    list_display: list[str] = (
         "id",
         "departure_airport",
         "arrival_airport",
@@ -108,7 +109,7 @@ class BookingAdmin(admin.ModelAdmin):
         "total_fare_with_vnd",
         "booking_date",
     )
-    list_filter = [
+    list_filter: list[str] = [
         "flight__departure_time",
         "flight__departure_airport",
         "flight__arrival_airport",
@@ -119,42 +120,42 @@ class BookingAdmin(admin.ModelAdmin):
         queryset = super().get_queryset(request)
         return queryset.prefetch_related("passengers")
 
-    date_hierarchy = "flight__departure_time"
+    date_hierarchy: str = "flight__departure_time"
 
-    list_per_page = 20
+    list_per_page: int = 20
 
     @staticmethod
-    def departure_airport(obj):
+    def departure_airport(obj: Booking) -> Airport:
         return obj.flight.departure_airport
 
     @staticmethod
-    def arrival_airport(obj):
+    def arrival_airport(obj: Booking) -> Airport:
         return obj.flight.arrival_airport
 
     @staticmethod
-    def passenger_names(obj):
+    def passenger_names(obj: Booking) -> str:
         return ", ".join([passenger.name for passenger in obj.passengers.all()])
 
     @staticmethod
-    def aircraft_code(obj):
+    def aircraft_code(obj: Booking) -> str:
         return obj.flight.aircraft.code
 
     @staticmethod
-    def departure_time(obj):
+    def departure_time(obj: Booking) -> str:
         departure_time = obj.flight.departure_time.astimezone()
         return departure_time.strftime("%Y-%m-%d %H:%M")
 
     @staticmethod
-    def arrival_time(obj):
+    def arrival_time(obj: Booking) -> str:
         arrival_time = obj.flight.arrival_time.astimezone()
         return arrival_time.strftime("%Y-%m-%d %H:%M")
 
     @staticmethod
-    def quantity(obj):
+    def quantity(obj: Booking) -> int:
         return obj.passengers.count()
 
     @staticmethod
-    def total_fare_with_vnd(obj):
+    def total_fare_with_vnd(obj: Booking) -> str:
         formatted_amount = "{:,.0f}".format(obj.total_fare)  # Format with commas for thousands separators
         return f"{formatted_amount} VND"
 
@@ -169,7 +170,7 @@ class BookingAdmin(admin.ModelAdmin):
     class Media:
         js = ("aviation/booking.js",)
 
-    def has_change_permission(self, request, obj=None):
+    def has_change_permission(self, request, obj: Union[Booking, None] = None):
 
         logger.debug(
             f"has_change_permission request {request} obj {obj} user {request.user} is supper user {request.user.is_superuser}"
@@ -183,7 +184,7 @@ class BookingAdmin(admin.ModelAdmin):
 
         return super().has_change_permission(request, obj)
 
-    def has_delete_permission(self, request, obj=None):
+    def has_delete_permission(self, request, obj: Union[Booking, None] = None):
 
         logger.debug(
             f"has_delete_permission request {request} obj {obj} user {request.user} is supper user {request.user.is_superuser}"
